b0bdf50e1797ae208fa8b886586b4f99671fea19
nhl1991 <laslark1991@gmail.com>
2025-03-27 18:29:33 +0900

    deleted /provider. Add hover css to navigation in /detail

--- src/app/detail/[id]/layout.tsx
+++ src/app/detail/[id]/layout.tsx
@@ -21,10 +21,10 @@ export default async function DetailLayout({
 
             <nav className="w-full bg-black">
                 <ul className="w-full p-2 flex text-white">
-                    <li className="p-2"><Link href={`/`}>&larr;BACK</Link></li>
-                    <li className="p-2"><Link href={`#detail`}>Detail</Link></li>
-                    <li className="p-2"><Link href={`#video`}>Video</Link></li>
-                    <li className="p-2"><Link href={`#provider`}>Provider</Link></li>
+                    <li className="p-2 hover:text-slate-500"><Link href={`/`}>&larr;BACK</Link></li>
+                    <li className="p-2 hover:text-slate-500"><Link href={`#detail`}>Detail</Link></li>
+                    <li className="p-2 hover:text-slate-500"><Link href={`#video`}>Video</Link></li>
+                    <li className="p-2 hover:text-slate-500"><Link href={`#provider`}>Provider</Link></li>
                 </ul>
             </nav>
             {children}
