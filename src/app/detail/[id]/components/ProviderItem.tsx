db834dc6163bb338d2604a211945a52804e83dff
nhl1991 <laslark1991@gmail.com>
2025-03-28 12:30:29 +0900

    update Provider Item size.

--- src/app/detail/[id]/components/ProviderItem.tsx
+++ src/app/detail/[id]/components/ProviderItem.tsx
@@ -4,7 +4,7 @@ import Image
 export default function ProviderItem({ item, image }: { item: Provider; image: string }) {
 
     return (
-        <div className="flex-shrink-0 border-2 border-amber-200 md:w-[90px] w-[70px] h-[70px] md:h-[90px] flex flex-col items-center justify-center">
+        <div className="flex-shrink-0 md:w-[90px] w-[70px] h-[70px] md:h-[90px] flex flex-col items-center justify-center">
             <div className=" md:w-[80px] w-[50px] md:h-[80px] h-[50px]">
                 <Image className="rounded-xl" src={`https://image.tmdb.org/t/p/w300/${image}`} width={80} height={80} alt={item.provider_name} />
 
